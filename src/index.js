e220c9ecf965a94621bcafed2dea312c7ba7386f
Nmacroy <nmacroy@gmail.com>
2020-02-24 23:58:42 -0600

    new file:   .gitignore 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/App.test.js 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/logo.svg 	new file:   src/serviceWorker.js 	new file:   src/setupTests.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+    <div>
+        <label class="label" for="name">Enter name:</label>
+        <input id="name" type="text"/>
+        <button style="background-color: blue;color: white;"> Submit</button>
+    </div>
+    )};
+
+ReactDOM.render(<App />,
+    document.querySelector('#root'));
+
